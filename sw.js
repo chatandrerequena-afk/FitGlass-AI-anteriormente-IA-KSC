3a7d0bee3fac3ba69083537021d94ca716ea0031
chatandrerequena-afk <chat.andrerequena@gmail.com>
2026-08-27 21:13:33 -0500

    Delete sw.js

--- sw.js
+++
@@ -1,3 +0,0 @@
-const CACHE='fitglass-v1';
-self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./styles.css','./app.js','./config.js','./manifest.json']))));
-self.addEventListener('fetch',event=>event.respondWith(caches.match(event.request).then(r=>r||fetch(event.request).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(event.request,copy));return resp}).catch(()=>caches.match('./index.html')))));
